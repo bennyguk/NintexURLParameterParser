b6f810b7a2245653474003bf0a56a21c0bb660fe
bennyguk <benny_guk@hotmail.com>
2021-07-29 11:17:44 +0100

    Update NintexURLParameterParser.js

--- NintexURLParameterParser.js
+++ NintexURLParameterParser.js
@@ -1,33 +1,33 @@
 <script type="text/javascript">
 
-// A script to collect parameter values from the URL and set WorkItemID and Analyst field values in the form.
+// A script to collect parameter values from the URL and set workItemID and analyst field values in the form.
 
 // Wait for the form to load before firing the script. Load parameters and values from the URL.
 window.onload = function () {
     var params = getUrlParams();
 
-    // Set the value of WorkItemID form field from the URL parameters.
+    // Set the value of workItemID form field from the URL parameters.
     var workItemID = document.querySelector('[id^="ctl00_ctl39_g_4cf3b399_e5c3_438f_82a8_e047accc5f5c_ctl00_ListForm2_formFiller_FormView_ctl04_ctl01_ctl03"]').id;
 
-    // If the Analyst parameter is missing, set the value to a zero length string.
-    if (getUrlParams().WorkItemID == null) {
+    // If the workItemID parameter is missing, set the value to a zero length string.
+    if (getUrlParams().workItemID == null) {
         document.getElementById(workItemID).value = "";
     }
     else {
-        document.getElementById(workItemID).value = getUrlParams().WorkItemID;
+        document.getElementById(workItemID).value = getUrlParams().workItemID;
     }
-    // Sets the WorkItemID field to read only (Could not get this to work properly in Nintex, easy in JScript).
+    // Sets the workItemID field to read only (Could not get this to work properly in Nintex, easy in JScript).
     document.getElementById(workItemID).readOnly = True;
 
-    // Set the value of WorkItemID form field from the URL parameters.
+    // Set the value of analyst form field from the URL parameters.
     var analyst = document.querySelector('[id^="ctl00_ctl39_g_4cf3b399_e5c3_438f_82a8_e047accc5f5c_ctl00_ListForm2_formFiller_FormView_ctl04_ctl02_ctl03"]').id;
 
-    // If the Analyst parameter is missing, set the value to an empty string.
-    if (getUrlParams().Analyst == null) {
+    // If the analyst parameter is missing, set the value to an empty string.
+    if (getUrlParams().analyst == null) {
         document.getElementById(analyst).value = "";
     }
     else {
-        document.getElementById(analyst).value = getUrlParams().Analyst;
+        document.getElementById(analyst).value = getUrlParams().analyst;
     }
     // Sets the Analyst field to read only (Could not get this to work properly in Nintex, easy in JScript).
     document.getElementById(analyst).readOnly = True;
